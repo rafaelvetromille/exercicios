70362a9280f4d7da4995e0711e33a9b49e3ac463
rafaelvetromille1@gmail.com <rafaelvetromille1@gmail.com>
2020-09-17 22:00:01 -0300

    nota

--- nota_corretagem.R
+++ nota_corretagem.R
@@ -1,15 +1,19 @@
+## 1. Pacotes necessários.
 library(tidyverse)
 library(stringr)
 library(readtext)
 
+## 2. Ler arquivos pdf na pasta.
 arquivos <- list.files(pattern = "nota[0-9]{1,2}\\.pdf$")
 
+## 3. Objeto principal de apoio.
 out = purrr::map_dfr(.x = arquivos, .f = readtext) %>%
   select(text) %>%
   pull() %>%
   str_squish() %>%
   str_to_lower()
 
+## 4. Construindo um tibble a partir dos dados.
 df <- tibble(
 
   # Identificador da Nota
@@ -36,6 +40,7 @@ df <- tibble(
 ) %>%
   unnest(cv)
 
+## 4. Título (ações)
 titulo = out %>%
   str_extract("(?<=d/c ).+(?= resumo dos negócios)") %>%
   str_split("1-") %>%
@@ -46,6 +51,7 @@ titulo = out %>%
   purrr::map_dfr(~ as_tibble(., .name_repair = "minimal")) %>%
   magrittr::set_colnames('acao')
 
+## 5. Quantidade, preço e valor.
 valores = out %>%
   str_extract("(?<=d/c ).+(?= resumo dos negócios)") %>%
   str_split("1-") %>%
@@ -57,7 +63,7 @@ valores = out %>%
   mutate(across(.cols = where(is.character),
                 .fns = ~ parse_number(.x, locale = locale(decimal_mark = ","))))
 
-
+## 6. Resultado final
 final <- bind_cols(df, titulo, valores) %>%
   mutate(across(where(is.character), str_to_upper)) %>%
   rename_all(toupper)
